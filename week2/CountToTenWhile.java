599b4bd68a35e40316a65c17c08e4d077558d069
jhmarchand <jhmarchand@users.noreply.github.com>
2017-07-13 13:32:47 -0400

    Add files via upload

---
+++ week2/CountToTenWhile.java
@@ -0,0 +1,14 @@
+
+public class CountToTenWhile {
+	
+	public static void main(String[] args) {
+		
+		int counter = 1;
+		System.out.println("Count to 10 by while");
+		
+		while ( counter < 11 ) {
+			System.out.println(counter++);		
+		}
+	}
+	
+}
