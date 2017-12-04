89e0e197c7c18411e616ae2063768300612a0f97
jhmarchand <jhmarchand@users.noreply.github.com>
2017-12-04 14:53:56 -0500

    Add files via upload

---
+++ Luke3/RedBackLinearAuto.java
@@ -0,0 +1,282 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+
+@Autonomous(name="Red back linear", group="Pushbot")
+
+public class RedBackLinearAuto extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+
+    double right = 0;
+    RelicRecoveryVuMark vuMark;
+    double CypherValue;
+    boolean Blue = false;
+    double backwards = 0;
+    double max = 1.0;
+    // Declare OpMode members.
+    private RelicRobot9087 robot = new RelicRobot9087();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
+
+        // Step 1:  Drive forward for 3 seconds
+        double driveSpeed = 0.4;
+        runtime.reset();
+
+        robot.squeeze();
+        robot.ballSensorServo2.setPosition(0.5);
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Detecting Ball %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 2:  Spin right for 1.3 seconds
+
+        robot.forkLifterDcMotor.setPower(-0.75);
+        robot.ballSensorServo2.setPosition(0.5);
+        robot.squeeze();
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "lifting block %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            vuMark = robot.vuforiaScan();
+            if (vuMark == RelicRecoveryVuMark.RIGHT){
+                CypherValue = -0.3;
+                right = 0.2;
+            }
+            else if (vuMark == RelicRecoveryVuMark.LEFT){
+                CypherValue = 0.33;
+            }
+
+            telemetry.addData("VuMark", "%s visible", vuMark);
+        }
+
+        // Step 3:  Drive Backwards for 1 Second
+        robot.forkLifterDcMotor.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.6)) {
+            telemetry.addData("Path", "Color Detected! Driving %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            if (robot.seeBlue(true) == false || Blue == true) {
+                robot.DriveMecanum(0, driveSpeed, 0.05);
+                Blue = true;
+            }
+            else {
+                robot.DriveMecanum(0, driveSpeed * -0.5, 0);
+                backwards = 1.2;
+                Blue = false;
+            }
+
+        }
+        robot.ballSensorServo2.setPosition(1.0);
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Raising Sensor %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,driveSpeed,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.25 + backwards + CypherValue)) {
+            telemetry.addData("Path", "Driving %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.5)) {
+            telemetry.addData("Path", "Slowing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        driveSpeed = 0.5;
+        robot.DriveMecanum(0, 0, driveSpeed*-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.1)) {
+            telemetry.addData("Path", "Rotating %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,driveSpeed * 0.1,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.25)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.open();
+        robot.DriveMecanum(0,0,0);
+        robot.forkLifterDcMotor.setPower(0.75);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.9)) {
+            telemetry.addData("Path", "Lowering Forklift %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.forkLifterDcMotor.setPower(0);
+        robot.DriveMecanum(0,driveSpeed,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.4)) {
+            telemetry.addData("Path", "Driving %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Slowing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,driveSpeed*-1,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.2)) {
+            telemetry.addData("Path", "Backing up %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.1)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,driveSpeed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.2)) {
+            telemetry.addData("Path", "Rotating %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,driveSpeed,0);
+        robot.partial();
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Vroom Vroom! Driving %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        robot.squeeze();
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Getting Block %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,driveSpeed*-1,0);
+        robot.forkLifterDcMotor.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.2)) {
+            telemetry.addData("Path", "Backing up %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.2)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,0,driveSpeed);
+        robot.forkLifterDcMotor.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.2 + right)) {
+            telemetry.addData("Path", "Rotating %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,0,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,driveSpeed,0);
+        robot.forkLifterDcMotor.setPower(-0.75);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Vroom Vroom! Driving %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+
+        }
+        robot.DriveMecanum(0,0,0);
+        robot.forkLifterDcMotor.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Processing %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.open();
+        robot.DriveMecanum(0,driveSpeed*-1,0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Dropping! %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.DriveMecanum(0,0,0);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+    }
+}
---
+++ Luke3/TestTeleOpMode_Iterative.java
@@ -0,0 +1,132 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@TeleOp(name="Test: Teleop Iterative OpMode", group="Iterative Opmode")
+public class TestTeleOpMode_Iterative extends OpMode
+{
+
+    boolean claw = false;
+    double max = 1.0;
+    // Declare OpMode members.
+    private RelicRobot9087 robot = new RelicRobot9087();
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        robot.init(hardwareMap);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        //if (gamepad1.right_trigger > 0)
+            //robot.ballSensorServo.setPosition(gamepad1.right_trigger);
+        //robot.ballSensorServo.setPosition(0.4);
+       // robot.ballSensorServo2.setPosition(1.0);
+        if (gamepad2.dpad_down){
+            robot.manClawServo.setPosition(1);
+            claw = false;
+        }
+        else if (gamepad2.dpad_up){
+            robot.manClawServo.setPosition(0);
+            claw = true;
+        }
+
+        robot.horizontalSlideDcMotor.setPower(gamepad2.right_stick_y);
+
+        if (gamepad2.right_bumper){
+            robot.manAngleServo.setPosition(0);
+        }
+        else if (gamepad2.left_bumper){
+            robot.manAngleServo.setPosition(1);
+        }
+
+        if ( gamepad1.a)
+            max = 1.0;
+        if ( gamepad1.b)
+            max = 0.25;
+        if ( gamepad1.x)
+            max = 0.75;
+        if ( gamepad1.y)
+            max = 0.5;
+
+        if (gamepad2.a)
+            robot.squeeze();
+        else if (gamepad2.y) {
+            robot.partial();
+        }
+        else if (gamepad2.b)
+            robot.open();
+
+      // robot.ballSensorServo2.setPosition(gamepad2.right_stick_y);
+        //robot.ballSensorServo.setPosition(1);
+
+       telemetry.addData("servo2", robot.ballSensorServo2.getPosition());
+
+        if (gamepad2.right_trigger >0 )
+            robot.forkLifterDcMotor.setPower(gamepad2.right_trigger *-1);
+        else if (gamepad2.left_trigger >0)
+            robot.forkLifterDcMotor.setPower(gamepad2.left_trigger);
+        else
+            robot.forkLifterDcMotor.setPower(gamepad2.left_stick_y);
+
+       // String ballColor = "I DON'T KNOW!!!";
+//        if (robot.colorSensor.red() > 15 && robot.colorSensor.red() > robot.colorSensor.blue())
+       // if ( robot.seeRed())
+            //ballColor = "red";
+       // if (robot.colorSensor.blue() > 15 && robot.colorSensor.blue() > robot.colorSensor.red())
+       // if ( robot.seeBlue() )
+           // ballColor = "blue";
+       // telemetry.addData("ballColor", ballColor);
+
+        robot.DriveMecanum(gamepad1.left_stick_x*max,-gamepad1.left_stick_y*max, gamepad1.right_stick_x*max*-1);
+
+       telemetry.addData("Color", robot.colorSensor.red());
+       telemetry.addData("Color", robot.colorSensor.blue());
+        telemetry.addData("Color", robot.colorSensor.green());
+      // telemetry.addData("color", robot.colorSensor.alpha());
+       telemetry.addData("Distance (cm)", robot.distanceSensor.getDistance(DistanceUnit.CM));
+       // robot.DriveTank(-gamepad2.left_stick_y, -gamepad2.right_stick_x);
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
